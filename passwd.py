5d8c8aeba800eb20bc96bb3b6db3603687d52cbc
Sergio Carro <sergioca22@gmail.com>
2015-03-09 19:08:46 +0100

    terminado2

--- passwd.py
+++ passwd.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 
+
 fichero = open('/etc/passwd','r')
 lectura = fichero.readlines()
 fichero.close()
 
-for linea in lectura:
-	shell = linea.split(":")[-1]
-	usuario = linea.split(":")[0]
-	print usuario, shell
 
-print lectura
-print "Hay "+ str(len(lectura))+ " usuarios"
+for linea in lectura
+    shell = linea.split(":")[-1]
+    usuario = linea.split(":)[0]
+    print usuario, shell
+
+print "Hay " + str(len(lectura)) + " usuarios"
